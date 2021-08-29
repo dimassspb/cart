bcd6dcd3fe1107d5bdc905499935c7021e586d42
Dmitrii Isakov <dimass82@mail.ru>
2021-08-29 13:06:51 +0300

    Composition

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,48 +1,43 @@
-import React, { useState } from 'react';
-const Counter = () => {
-  const [count, setCount] = useState(0);
-  const tags = ['tag1', 'tag2', 'tag3'];
+import React from 'react';
 
-  const formCount = () => {
-    return count === 0 ? 'Ноль' : count;
+const Counter = ({ id, value, name, onIncrement, onDecrement, onDelete }) => {
+  
+  const formValue = () => {
+    return value === 0 ? 'Ноль' : value;
   };
 
   const getBadgeClasses = () => {
     let classes = 'badge m-2 bg-';
-    classes += count === 0 ? 'danger' : 'primary';
+    classes += value === 0 ? 'danger' : 'primary';
     return classes;
-  };
-
-  const renderTags = () => {
-    if (tags.length === 0) {
-      return 'Тегов не найдено';
-    }
-    return tags.map((tag) => <li key={tag}>{tag}</li>);
-  };
-
-  const handleIncrement = () => {
-    setCount(count + 1);
-  };
-
-  const handleDecrement = () => {
-    if (count > 0) {
-      setCount(count - 1);
-    }
   };
 
   return (
     <>
-      {renderTags()}
-      <span className={getBadgeClasses()}>{formCount()}</span>
-      <button
-        onClick={handleIncrement}
-        className="btn btn-secondary btn-sm m-2"
-      >
-        Increment
-      </button>
-      <button onClick={handleDecrement} className="btn btn-secondary btn-sm">
-        Decrement
-      </button>
+      <div>
+        <h4>{name}</h4>
+        <span className={getBadgeClasses()}>{formValue()}</span>
+        <button
+          onClick={() => onIncrement(id)}
+          className="btn btn-secondary btn-sm m-1"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => onDecrement(id)}
+          className="btn btn-secondary btn-sm"
+        >
+          Decrement
+        </button>
+        <button
+          className="btn btn-sm btn-danger m-2"
+          onClick={() => {
+            onDelete(id);
+          }}
+        >
+          Delete
+        </button>
+      </div>
     </>
   );
 };
